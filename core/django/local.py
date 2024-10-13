3452b376e1debc7f03f9402a2ff6be84dc11793b
mateusz-wroblewski-lab <155849345+mateusz-wroblewski-lab@users.noreply.github.com>
2024-10-14 00:23:45 +0200

    Update local.py

--- core/django/local.py
+++ core/django/local.py
@@ -11,20 +11,27 @@ ROOT_URLCONF = 'core.urls'
 ALLOWED_HOSTS = ['localhost', 'crmtiw.studioshouse.com', 'www.crmtiw.studioshouse.com']
 CSRF_TRUSTED_ORIGINS = ['https://crmtiw.studioshouse.com', 'http://crmtiw.studioshouse.com']
 
-# SECURE_SSL_REDIRECT = False # if True "You're accessing the development server over HTTPS, but it only supports HTTP."
 
+#X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_HSTS_SECONDS = 86400
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = False # if True "You're accessing the development server over HTTPS, but it only supports HTTP."
 
-SESSION_COOKIE_AGE = 30000
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_AGE = 300
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 AUTO_LOGOUT = {
-    'IDLE_TIME': 30000,
+    'IDLE_TIME': 300,
     'REDIRECT_TO_LOGIN_IMMEDIATELY': True,
     'MESSAGE': 'Sesja wygasła. Proszę zalogować się ponownie.',
     } 
@@ -55,14 +62,19 @@ INSTALLED_APPS = [
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / 'core/db/backup'}
 
-# Local database
-DATABASES = {
+
+DATABASES = { 
+    # Local database
     'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'core/db/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DBU'), # name of databsae
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     },
 
-# Neon database
+    # Neon database
     'neon_db': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('PGDATABASE'),
@@ -72,19 +84,7 @@ DATABASES = {
         'PORT': 5432,
         'OPTIONS': {'sslmode': 'require',},
     },
-
-# Postgres Unraid database
-    'postgres_db': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB'), # name of databsae
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': env('POSTGRES_PORT'),
-    },
 }
-
-
 
 DATABASE_ROUTERS = [
     # 'core.settings.routers.CloudAppRouter',
@@ -92,37 +92,9 @@ DATABASE_ROUTERS = [
     'core.settings.routers.NeonAppRouter',
     ]
 
-# Cloudflare storages
-# AWS_STORAGE_BUCKET_NAME = env('BUCKET_NAME')
-# AWS_S3_ENDPOINT_URL = env('BUCKET_ID')
-# AWS_S3_ACCESS_KEY_ID = env('BUCKET_ACCES_KEY')
-# AWS_S3_SECRET_ACCESS_KEY = env('BUCKET_SECRET_KEY')
-# AWS_S3_SIGNATURE_VERSION = 's3v4'
-# AWS_S3_FILE_OVERWRITE = False
-# STORAGES = {
-#     # Media file (image) management   
-#     "default": {
-#         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-#     },
-#     # CSS and JS file management
-#     "staticfiles": {
-#         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-#     },
-# }
-
-# Hcaptcha
-HCAPTCHA_SITEKEY = env('HCAPTCHA_SITEKEY')
-HCAPTCHA_SECRET = env('HCAPTCHA_SECRET')
-HCAPTCHA_TIMEOUT = 5
-
 # Cloudflare
 TURNSTILE_SITEKEY = env('TURNSTILE_SITEKEY')
 TURNSTILE_SECRET = env('TURNSTILE_SECRET')
 TURNSTILE_JS_API_URL = 'https://challenges.cloudflare.com/turnstile/v0/api.js'
 TURNSTILE_VERIFY_URL = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
 TURNSTILE_TIMEOUT = 5
-
-# Google
-RECAPTCHA_PUBLIC_KEY = env('PUBLIC_KEY')
-RECAPTCHA_PRIVATE_KEY = env('PRIVATE_KEY')
-# RECAPTCHA_USE_SSL = True
