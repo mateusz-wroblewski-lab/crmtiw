dd555e94db838e2b9258256f2a3d350be92fcedf
mateusz-wroblewski-lab <155849345+mateusz-wroblewski-lab@users.noreply.github.com>
2024-11-06 15:46:10 +0100

    Update routers.py

--- core/settings/routers.py
+++ core/settings/routers.py
@@ -1,5 +1,5 @@
 class LocalAppRouter:
-    route_app_labels = ['patients']
+    route_app_labels = ['crm']
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
@@ -42,28 +42,4 @@ class NeonAppRouter:
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
             return db == 'neon_db'
-        return None
-
-
-class CloudAppRouter:
-    route_app_labels = ['patients']
-
-    def db_for_read(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return 'cloudfl_db'
-        return None
-    
-    def db_for_write(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return 'cloudfl_db'
-        return None
-    
-    def allow_relations(self, obj1, obj2, **hints):
-        if obj1._meta.app_label in self.route_app_labels and obj2._meta.app_label in self.route_app_labels:
-            return True
-        return None
-    
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label in self.route_app_labels:
-            return db == 'cloudfl_db'
         return None
